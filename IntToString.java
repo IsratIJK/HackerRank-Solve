d1e1f9d5d186df8fdc7abcc516958adf48ab853f
IsratIJK <iptu.jahan.amin@gmail.com>
2021-06-20 12:30:31 +0600

    Solve IntToString

---
+++ IntToString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n>=-100 && n<=100){
+            System.out.println("Good job");
+        }
+
+    }
+}
